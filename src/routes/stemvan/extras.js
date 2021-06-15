08b57af17124434c35ab7326538141e73f350a86
Niels Vegter <niels@denes.nl>
2021-06-15 14:32:05 +0100

    Add rattenoverlast to stemvanwest

--- src/routes/stemvan/extras.js
+++ src/routes/stemvan/extras.js
@@ -11,13 +11,23 @@ module.exports = function( app ) {
 			};
 			res.out('locatie', false, data);
 		}));
+
+	  app.get('/wedstrijd', co.wrap(function*( req, res, next ) {
+		  var data = {
+		  };
+		  res.out('wedstrijd', false, data);
+	  }));
+
 	}
 
-		app.get('/wedstrijd', co.wrap(function*( req, res, next ) {
-			var data = {
-			};
-			res.out('wedstrijd', false, data);
-		}));
+	if (config.siteId = 'stemvanwest') {
+
+	  app.get('/rattenoverlast', co.wrap(function*( req, res, next ) {
+		  var data = {
+		  };
+		  res.out('rattenoverlast', false, data);
+	  }));
+
 	}
 
 };
